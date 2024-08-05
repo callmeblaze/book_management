383408418b58f1b21c07d2aa3db77d14842e1ec1
Ranjith N <ranjith.blaze@gmail.com>
2024-08-05 14:07:33 +0530

    Update config.py with aws config

--- config.py
+++ config.py
@@ -7,6 +7,6 @@
 # config.py
 
 DATABASE_USERNAME = 'fastapi_admin'
-DATABASE_PASSWORD = 'Callmeblaze!05' 
+DATABASE_PASSWORD = 'Callmeblaze!05'
 DATABASE_HOST = 'fastapi-db.cjsq2wcs4e81.eu-north-1.rds.amazonaws.com'
-DATABASE_NAME = 'fastapi_db'
+DATABASE_NAME = 'fastapi-db'
