477c1072724304936d61ac8d16d16aff37185d21
Ranjith N <ranjith.blaze@gmail.com>
2024-08-05 13:50:50 +0530

    Update config.py with aws config

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+# # config.py
+# DATABASE_USERNAME = "postgres"
+# DATABASE_PASSWORD = "root"
+# DATABASE_HOST = "localhost"
+# DATABASE_NAME = "BM_db"
+
 # config.py
-DATABASE_USERNAME = "postgres"
-DATABASE_PASSWORD = "root"
-DATABASE_HOST = "localhost"
-DATABASE_NAME = "BM_db"
+
+DATABASE_USERNAME = 'fastapi_admin'
+DATABASE_PASSWORD = 'Callmeblaze!05' 
+DATABASE_HOST = 'fastapi-db.cjsq2wcs4e81.eu-north-1.rds.amazonaws.com'
+DATABASE_NAME = 'fastapi_db'
