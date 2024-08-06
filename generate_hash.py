4c11c98c293f9db511fa056e51b1264f221a8674
Ranjith N <ranjith.blaze@gmail.com>
2024-08-06 12:19:46 +0530

    updated config and generate_hash

--- generate_hash.py
+++ generate_hash.py
@@ -1,10 +1,11 @@
 from passlib.context import CryptContext
+import config
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-password = "gogreen"
+password = config.PASSWORD
 hashed_password = hash_password(password)
 print(f"Hashed password: {hashed_password}")
